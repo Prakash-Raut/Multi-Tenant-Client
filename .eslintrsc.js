5067d5039348189e98ecaffba9f9453e8fdee3a2
Prakash Raut <prakash_raut@outlook.com>
2025-01-09 21:11:35 +0530

    add: husky prettier eslint

---
+++ .eslintrsc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "plugin:prettier/recommended", // Enables Prettier integration
+  ],
+  rules: {
+    // Customize rules if needed
+    "prettier/prettier": ["error"],
+  },
+};
