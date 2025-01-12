2e4e22c50a80089774e9099e72f76b032a7dc034
Prakash Raut <prakash_raut@outlook.com>
2025-01-12 16:03:22 +0530

    add: final touch product card

--- src/app/(home)/components/product-card.tsx
+++ src/app/(home)/components/product-card.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -14,6 +15,7 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import ToppingList from "./topping-list";
 
@@ -52,111 +54,114 @@ const ProductCard = ({ product }: PropTypes) => {
             Choose
           </DialogTrigger>
           <DialogContent className="max-w-3xl">
-            <DialogHeader>
-              <DialogTitle>Are you absolutely sure?</DialogTitle>
-              <DialogDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </DialogDescription>
-              <div className="flex justify-between">
-                <div className="w-1/3">
-                  <Image
-                    src={product.image}
-                    alt={product.name}
-                    width={450}
-                    height={450}
-                  />
+            <div className="flex items-center justify-between gap-4">
+              <div className="w-1/3">
+                <Image
+                  src={product.image}
+                  alt={product.name}
+                  width={450}
+                  height={450}
+                />
+              </div>
+              <div className="w-2/3">
+                <DialogHeader>
+                  <DialogTitle>{product.name}</DialogTitle>
+                  <DialogDescription>{product.description}</DialogDescription>
+                </DialogHeader>
+                <div>
+                  <h4 className="mt-6">Choose the size</h4>
+                  <RadioGroup
+                    defaultValue="small"
+                    className="mt-2 grid grid-cols-3 gap-4"
+                  >
+                    <div>
+                      <RadioGroupItem
+                        id="small"
+                        value="small"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="small"
+                        className="flex flex-col items-center justify-between rounded-md border-2 bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Small
+                      </Label>
+                    </div>
+                    <div>
+                      <RadioGroupItem
+                        id="medium"
+                        value="medium"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="medium"
+                        className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Medium
+                      </Label>
+                    </div>
+                    <div>
+                      <RadioGroupItem
+                        id="large"
+                        value="large"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="large"
+                        className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Large
+                      </Label>
+                    </div>
+                  </RadioGroup>
                 </div>
-                <div className="w-2/3">
-                  <div>
-                    <h3 className="text-xl font-bold">{product.name}</h3>
-                    <p className="mt-1">{product.description}</p>
-                  </div>
-                  <div>
-                    <h4 className="mt-6">Choose the size</h4>
-                    <RadioGroup
-                      defaultValue="small"
-                      className="mt-2 grid grid-cols-3 gap-4"
-                    >
-                      <div>
-                        <RadioGroupItem
-                          id="small"
-                          value="small"
-                          className="peer sr-only"
-                        />
-                        <Label
-                          htmlFor="small"
-                          className="flex flex-col items-center justify-between rounded-md border-2 bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                        >
-                          Small
-                        </Label>
-                      </div>
-                      <div>
-                        <RadioGroupItem
-                          id="medium"
-                          value="medium"
-                          className="peer sr-only"
-                        />
-                        <Label
-                          htmlFor="medium"
-                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                        >
-                          Medium
-                        </Label>
-                      </div>
-                      <div>
-                        <RadioGroupItem
-                          id="large"
-                          value="large"
-                          className="peer sr-only"
-                        />
-                        <Label
-                          htmlFor="large"
-                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                        >
-                          Large
-                        </Label>
-                      </div>
-                    </RadioGroup>
-                  </div>
-                  <div>
-                    <h4 className="mt-6">Choose the crust</h4>
-                    <RadioGroup
-                      defaultValue="thin"
-                      className="mt-2 grid grid-cols-3 gap-4"
-                    >
-                      <div>
-                        <RadioGroupItem
-                          id="thin"
-                          value="thin"
-                          className="peer sr-only"
-                        />
-                        <Label
-                          htmlFor="thin"
-                          className="flex flex-col items-center justify-between rounded-md border-2 bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                        >
-                          Thin
-                        </Label>
-                      </div>
-                      <div>
-                        <RadioGroupItem
-                          id="thick"
-                          value="thick"
-                          className="peer sr-only"
-                        />
-                        <Label
-                          htmlFor="thick"
-                          className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
-                        >
-                          Thick
-                        </Label>
-                      </div>
-                    </RadioGroup>
-                  </div>
-                  <ToppingList />
+                <div>
+                  <h4 className="mt-6">Choose the crust</h4>
+                  <RadioGroup
+                    defaultValue="thin"
+                    className="mt-2 grid grid-cols-3 gap-4"
+                  >
+                    <div>
+                      <RadioGroupItem
+                        id="thin"
+                        value="thin"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="thin"
+                        className="flex flex-col items-center justify-between rounded-md border-2 bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Thin
+                      </Label>
+                    </div>
+                    <div>
+                      <RadioGroupItem
+                        id="thick"
+                        value="thick"
+                        className="peer sr-only"
+                      />
+                      <Label
+                        htmlFor="thick"
+                        className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+                      >
+                        Thick
+                      </Label>
+                    </div>
+                  </RadioGroup>
+                </div>
+                <ToppingList />
+                <div className="mt-12 flex items-center justify-between">
+                  <span className="text-lg font-bold">
+                    &#8377;
+                    {product.price}
+                  </span>
+                  <Button>
+                    <ShoppingCart size={20} />
+                    <span>Add to cart</span>
+                  </Button>
                 </div>
               </div>
-            </DialogHeader>
+            </div>
           </DialogContent>
         </Dialog>
       </CardFooter>
