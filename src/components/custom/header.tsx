94896526607bea1b2e105fee03b996cc4fb1fb1d
Prakash Raut <prakash_raut@outlook.com>
2025-02-09 14:33:55 +0530

    add: tenant data in select box

--- src/components/custom/header.tsx
+++ src/components/custom/header.tsx
@@ -5,12 +5,31 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { api } from "@/lib/config";
+import { Tenant } from "@/types";
 import { Phone, ShoppingBasket } from "lucide-react";
 import Link from "next/link";
 import Logo from "../icons/logo";
 import { Button } from "../ui/button";
 
-const Header = () => {
+const Header = async () => {
+  const tenantResponse = await fetch(
+    `${api}/api/auth/tenants?perPage=100&currentPage=1`,
+    {
+      next: {
+        revalidate: 3600, // 1 hour
+      },
+    }
+  );
+
+  if (!tenantResponse.ok) {
+    throw new Error("Failed to fetch tenants");
+  }
+
+  const tenants: { data: Tenant[] } = await tenantResponse.json();
+
+  console.log(tenants);
+
   return (
     <header>
       <nav className="container mx-auto flex items-center justify-between px-24 py-6">
@@ -21,9 +40,11 @@ const Header = () => {
               <SelectValue placeholder="Select Restaurant" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="cheesy-delight">Cheesy Delight</SelectItem>
-              <SelectItem value="pizza-hut">Pizza Hut</SelectItem>
-              <SelectItem value="kids-corner">Kids Corner</SelectItem>
+              {tenants.data.map((tenant) => (
+                <SelectItem key={tenant.id} value={String(tenant.id)}>
+                  {tenant.name}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
         </div>
