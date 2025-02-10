3cae1e168a6175e2a20710f401a29ec39e8dd3f9
Prakash Raut <prakash_raut@outlook.com>
2025-02-10 17:11:49 +0530

    feat: optimize product and category fetching with concurrent requests

--- src/app/(home)/components/product-list.tsx
+++ src/app/(home)/components/product-list.tsx
@@ -4,42 +4,30 @@ import { Category, Product } from "@/types";
 import ProductCard from "./product-card";
 
 const ProductList = async () => {
-  // TODO: Add Concurrent Req -> Promise.all
-
-  const categoryResponse = await fetch(
-    `${api}/api/catalog/categories?perPage=100&currentPage=1`,
-    {
+  // TODO: Make TenantId Dynamic
+  const [categoryResponse, productResponse] = await Promise.all([
+    fetch(`${api}/api/catalog/categories?perPage=100&currentPage=1`, {
       next: {
         revalidate: 3600, // 1 hour
       },
-    }
-  );
+    }),
+    fetch(`${api}/api/catalog/products?perPage=100&currentPage=1&tenantId=10`, {
+      next: {
+        revalidate: 3600, // 1 hour
+      },
+    }),
+  ]);
 
   if (!categoryResponse.ok) {
     throw new Error("Failed to fetch categories");
   }
 
-  const categories: Category[] = await categoryResponse.json();
-
-  console.log(categories);
-
-  //TODO: ADD DYNAMIC TenantId
-  const productResponse = await fetch(
-    `${api}/api/catalog/products?perPage=100&currentPage=1&tenantId=10`,
-    {
-      next: {
-        revalidate: 3600, // 1 hour
-      },
-    }
-  );
-
   if (!productResponse.ok) {
     throw new Error("Failed to fetch products");
   }
 
+  const categories: Category[] = await categoryResponse.json();
   const products: { data: Product[] } = await productResponse.json();
-
-  console.log(products);
 
   return (
     <section className="container mx-auto flex items-center justify-between px-24 py-4">
