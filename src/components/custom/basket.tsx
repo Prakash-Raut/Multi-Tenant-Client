d5a879a234cf8fee659219f4ecb22a19a06750b8
Prakash Raut <prakash_raut@outlook.com>
2025-02-10 12:39:08 +0530

    feat: display the number of items in the shopping basket dynamically

--- src/components/custom/basket.tsx
+++ src/components/custom/basket.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useAppSelector } from "@/lib/store/hooks";
 import { ShoppingBasket } from "lucide-react";
 import Link from "next/link";
 
 const Basket = () => {
+  const cartItems = useAppSelector((state) => state.cart.cartItems);
   return (
     <>
       <div className="relative">
@@ -11,7 +13,7 @@ const Basket = () => {
           <ShoppingBasket size={20} />
         </Link>
         <span className="absolute -right-5 -top-4 inline-flex h-6 w-6 items-center justify-center rounded-full bg-primary font-bold text-white">
-          {10}
+          {cartItems.length}
         </span>
       </div>
     </>
