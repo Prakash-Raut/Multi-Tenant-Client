6cb8a391ed5ce57733169b3aded2d74af712bc51
Prakash Raut <prakash_raut@outlook.com>
2025-05-12 15:48:07 +0530

    fix: order page nav link

--- src/components/custom/nav-right.tsx
+++ src/components/custom/nav-right.tsx
@@ -2,12 +2,16 @@
 
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 const BasketWithoutSSR = dynamic(() => import("./basket"), {
 	ssr: false,
 });
 
 const NavRight = () => {
+	const searchParams = useSearchParams();
+	const restaurantId = searchParams.get("restaurantId");
+
 	return (
 		<>
 			<ul className="flex items-center space-x-4">
@@ -17,7 +21,10 @@ const NavRight = () => {
 					</Link>
 				</li>
 				<li className="inline-block">
-					<Link href="/orders" className="font-medium hover:text-primary">
+					<Link
+						href={`/orders?restaurantId=${restaurantId}`}
+						className="font-medium hover:text-primary"
+					>
 						Orders
 					</Link>
 				</li>
