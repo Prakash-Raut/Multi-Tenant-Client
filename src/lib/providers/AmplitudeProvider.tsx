eb21657aa9d2568367c09aeaecbdad11c10db7a6
Prakash Raut <prakash_raut@outlook.com>
2025-08-26 21:33:26 +0530

    feat: integrate session replay plugin with Amplitude analytics

--- src/lib/providers/AmplitudeProvider.tsx
+++ src/lib/providers/AmplitudeProvider.tsx
@@ -1,21 +1,26 @@
 "use client";
 
-import { init, track } from "@amplitude/analytics-browser";
+import * as amplitude from "@amplitude/analytics-browser";
+import { sessionReplayPlugin } from "@amplitude/plugin-session-replay-browser";
 import { type PropsWithChildren, createContext, useEffect } from "react";
 
-const AMPLITUDE_API_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
+const AMPLITUDE_API_KEY =
+	process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY ||
+	"fce0354d33d49be0f72471eb7b4dd1b4";
 
 export const AmplitudeContext = createContext({});
 
 const AmplitudeContextProvider = ({ children }: PropsWithChildren) => {
 	useEffect(() => {
-		init(AMPLITUDE_API_KEY as string, undefined, {
-			defaultTracking: { sessions: true },
-		});
+		// Add Session Replay to the Amplitude instance
+		amplitude.add(sessionReplayPlugin({ sampleRate: 1 }));
+
+		// Initialize amplitude with autocapture enabled
+		amplitude.init(AMPLITUDE_API_KEY, { autocapture: true });
 	}, []);
 
 	const trackAmplitudeEvent = (eventName: string, eventProperties: object) => {
-		track(eventName, eventProperties);
+		amplitude.track(eventName, eventProperties);
 	};
 
 	return (
